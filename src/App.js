d23451abd22bf229fae3ee8aa3e4601168483d25
Eros Rodriguez <elewites@gmail.com>
2021-12-31 02:39:56 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+//libraries
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+//styling
+import "./App.css";
 
 function App() {
+  //image url
+  const [image, setImage] = useState("");
+  //loading status
+  const [loading, setLoading] = useState(true);
+
+  //MODIFIES: image, loading
+  //EFFECTS: makes get request to /get-random-image-url,
+  //         sets image to response, and updates loading state
+  const updatePhoto = async () => {
+    setLoading(true);
+    //
+    try {
+      const response = await axios.get(
+        "http://localhost:3001/get-random-image-url"
+      );
+      setImage(response.data);
+      console.log(response.data);
+      //
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+      //
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //updatePhoto function will be called on each page refresh
+  useEffect(() => {
+    updatePhoto();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="button-div">
+        <button
+          onClick={() => {
+            updatePhoto();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Random Photo
+        </button>
+      </div>
+      <div className="image-div">
+        {loading ? <div class="loader"></div> : <img alt={1} src={image}></img>}
+      </div>
     </div>
   );
 }
